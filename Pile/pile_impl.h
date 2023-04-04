dab8eb717c399f677b3c678b8b075f9e0784d60e
calum <calum.quinn@heig-vd.ch>
2023-04-04 16:28:34 +0200

    Reste plus qu'un bout

--- Pile/pile_impl.h
+++ Pile/pile_impl.h
@@ -9,31 +9,32 @@
 template <typename T>
 Pile<T>::Pile(size_t n) {
    //Doit allouer la mémoire pour n éléments de type T (sans les construire)
-   Pile* pile = new Pile(n);
+   data = (T*)(::operator new[](n*sizeof(T)));
 }
 
 template <typename T>
 Pile<T>::~Pile() {
    //Détruire les éléments et libérer la mémoire
-   while (!this->empty()) {
-      ~(this->top());
-   }
+   delete[] data;
 }
 
 template <typename T>
 void Pile<T>::push(T e) {
-   //La méthode push doit construire un élément par construisant
+   //La méthode push doit construire un élément en construisant
    //par déplacement l'objet passé en paramètre à la bonne addresse mémoire.
+   *(data + taille) = e;
+   ++taille;
 }
 
 template <typename T>
 void Pile<T>::pop() {
    //pop doit détruire l'élément au sommet sans libérer de mémoire.
+   --taille;
 }
 
 template <typename T>
 const T& Pile<T>::top() const {
-   return top();
+   return *(data + taille - 1);
 }
 
 template <typename T>
@@ -51,13 +52,18 @@ Pile<T>::Pile(const Pile& other) {
    //Le constructeur de copie effectue une copie de la Pile reçue en
    // paramètre avec la même capacité que celle-ci.
    capacite = other.capacite;
-   this = other;
+   *this = other;
 }
 
 template <typename T>
 void Pile<T>::swap(Pile& other) noexcept {
    //La méthode swap échange le contenu des 2 piles en offrant une garantie noexcept.
    // Elle doit vous aider à mettre en oeuvre un des cas de l'opérateur d'affectation.
+   if (this != &other) {
+      swap(this->taille,other.taille);
+      swap(this->capacite,other.capacite);
+      swap(this->data,other.data);
+   }
 }
 
 template <typename T>
@@ -66,9 +72,25 @@ Pile<T>& Pile<T>::operator= ( const Pile& other ) {
    // si la taille de la Pile à copier est inférieure ou égale à la capacité
    // courante de la pile, on ne réalloue pas de mémoire.
    // Par contre, si la capacité actuelle est trop faible, on réalloue.
+   if (this == &other)
+      return *this;
+
+
+
    if (other.taille > this->capacite) {
       //Réallouer de la mémoire
+//      Pile temp{other};
+
+      T* temp = T(::operator new [](other.taille * sizeof(T)));
+      delete[] data;
+      data = temp;
+      taille = other.taille;
+//      swap(other);
    }
+
+   copy(other.data, other.data + other.taille, data);
+
+   return *this;
 }
 
 
